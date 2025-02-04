abb5023481ba743a4a00514bdaedaa3cb28584e5
Yatharth Sharma <yatharth0045@gmail.com>
2025-02-04 07:09:13 +0530

    Added visitor count

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+visitCount = 0
+
 @app.route('/')
 def mainapp():
-    return 'Hello, World!'
+    global visitCount
+    visitCount += 1
+    return 'Hello, World! This page has been visited {} times'.format(visitCount)  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8080)
-    
